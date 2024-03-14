1dfce77d346e8b3f4e48e0ce75e48cee4795e90c
adithyanp95 <padithyan@gmail.com>
2024-03-14 10:33:15 +0530

    v card phone number issue fixed

--- scripts.js
+++ scripts.js
@@ -59,8 +59,11 @@ document.addEventListener("DOMContentLoaded", async function () {
         document.querySelector(".description").textContent =
           userData.attributes.bio;
         document.querySelector(".profession").textContent =
-          userData.attributes.designation; // added the class selector here
-        userEmail = userData.attributes.email; // Assign email to the local variable
+          userData.attributes.designation;
+        userEmail = userData.attributes.email;
+
+        // Assign phone to a variable accessible in downloadVCard function
+        vcardPhone = userData.attributes.phone;
 
         // Call renderLinks function to initially render data
         renderLinks();
@@ -116,6 +119,7 @@ document.addEventListener("DOMContentLoaded", async function () {
     VERSION:3.0
     FN:${vcardName}
     ORG:${document.querySelector(".profession").textContent}
+    TEL;TYPE=CELL:${vcardPhone}
     EMAIL:${vcardEmail}
     END:VCARD`;
 
@@ -146,34 +150,105 @@ document.addEventListener("DOMContentLoaded", async function () {
 
 // Updated mapping for social media platforms including image path and link data
 const iconMap = {
-  facebook: {
-    imagePath: "https://tapseed.cloud/uploads/facebook_icon_6b6420ab3b.png",
-    link_data: "https://www.facebook.com/",
-  },
-  instagram: {
-    imagePath: "https://tapseed.cloud/uploads/instagram_icon_b9e86a2bd2.png",
-    link_data: "https://www.instagram.com/",
-  },
   whatsapp: {
     imagePath: "https://tapseed.cloud/uploads/whatsapp_icon_2c1255c072.png",
-    link_data: " https://wa.me/91",
-  },
-  email: {
-    imagePath: "https://tapseed.cloud/uploads/email_icon_d6d4621dbb.png",
-    link_data: "mailto:",
-  },
-  phone: {
-    imagePath: "https://tapseed.cloud/uploads/phone_icon_275497ff7c.png",
-    link_data: "tel:+91",
+    link_data: "https://wa.me/91",
   },
   website: {
     imagePath: "https://tapseed.cloud/uploads/browser_icon_aadc966123.png",
     link_data: "https://",
   },
-  gmb: {
+  phone: {
+    imagePath: "https://tapseed.cloud/uploads/phone_icon_275497ff7c.png",
+    link_data: "tel:",
+  },
+  facebook: {
+    imagePath: "https://tapseed.cloud/uploads/facebook_icon_6b6420ab3b.png",
+    link_data: "https://www.facebook.com/",
+  },
+
+  instagram: {
+    imagePath: "https://tapseed.cloud/uploads/instagram_icon_b9e86a2bd2.png",
+    link_data: "https://www.instagram.com/",
+  },
+  linkedin: {
+    imagePath: "https://tapseed.cloud/uploads/linkedin_deab9b8ad4.png",
+    link_data: "",
+  },
+  email: {
+    imagePath: "https://tapseed.cloud/uploads/email_icon_d6d4621dbb.png",
+    link_data: "mailto:",
+  },
+  message: {
+    imagePath: "https://tapseed.cloud/uploads/messages_icon_1901bda864.png",
+    link_data: "sms: ",
+  },
+
+  x: {
+    imagePath: "https://tapseed.cloud/uploads/twitter_0b0fd46945.png",
+    link_data: "",
+  },
+  snapchat: {
+    imagePath: "https://tapseed.cloud/uploads/snapchat_819240ca30.png",
+    link_data: "",
+  },
+  youtube: {
+    imagePath: "https://tapseed.cloud/uploads/youtube_ec54ba8a39.png",
+    link_data: "https://www.youtube.com/@",
+  },
+  "make my trip": {
+    imagePath: "https://tapseed.cloud/uploads/make_my_trip_88836c0b68.png",
+    link_data: "",
+  },
+
+  "google business": {
     imagePath: "https://tapseed.cloud/uploads/gmb_icon_4bbf255130.png",
     link_data: "",
   },
+  bookmyshow: {
+    imagePath: "https://tapseed.cloud/uploads/bookmyshow_a1353e1416.png",
+    link_data: "",
+  },
+  spotify: {
+    imagePath: "https://tapseed.cloud/uploads/spotify_icon_69ddbf03b3.png",
+    link_data: "link",
+  },
+  Soundcloud: {
+    imagePath: "https://tapseed.cloud/uploads/soundcloud_8760926e60.png",
+    link_data: "",
+  },
+
+  skype: {
+    imagePath: "https://tapseed.cloud/uploads/skype_d7fa5741bf.png",
+    link_data: "",
+  },
+  line: {
+    imagePath: "https://tapseed.cloud/uploads/line_10211a5995.png",
+    link_data: "",
+  },
+
+  facetime: {
+    imagePath: "https://tapseed.cloud/uploads/gmb_icon_4bbf255130.png",
+    link_data: "",
+  },
+  tiktok: {
+    imagePath: "https://tapseed.cloud/uploads/tiktok_f7557eb220.png",
+    link_data: "",
+  },
+  threads: {
+    imagePath: "https://tapseed.cloud/uploads/threads_9dbf12e3ae.png",
+    link_data: "",
+  },
+  likee: {
+    imagePath: "https://tapseed.cloud/uploads/likee_cff7699c95.png",
+    link_data: "",
+  },
+
+  tripadvisor: {
+    imagePath: "https://tapseed.cloud/uploads/tripadvisor_icon_e9e9ed8ea5.png",
+    link_data: "",
+  },
+
   // Add more mappings for other social media platforms as needed
 };
 
@@ -181,8 +256,11 @@ const iconMap = {
 function createLinkElement(linkData) {
   const { id, attributes } = linkData;
 
-  // Check if link_on is true, if not, return null
-  if (!attributes.link_on) {
+  // Check if attributes.link_name is valid before accessing its properties
+  const linkName = attributes.link_name && attributes.link_name.toLowerCase();
+
+  if (!linkName || !iconMap[linkName]) {
+    // Return null if linkName is null, undefined, or not found in iconMap
     return null;
   }
 
@@ -191,15 +269,14 @@ function createLinkElement(linkData) {
 
   const linkElement = document.createElement("a");
   // Prepend the link data associated with the social media platform to the link URL
-  linkElement.href =
-    iconMap[attributes.link_name.toLowerCase()].link_data + attributes.Link;
-  console.log(linkElement);
+  linkElement.href = iconMap[linkName].link_data + attributes.link;
+
   const imgElement = document.createElement("img");
   imgElement.className = "socialimages";
   imgElement.alt = attributes.link_name; // Set alt text to link name
 
   // Get the image path from the iconMap based on the social media platform
-  const imagePath = iconMap[attributes.link_name.toLowerCase()].imagePath;
+  const imagePath = iconMap[linkName].imagePath;
   if (imagePath) {
     imgElement.src = imagePath;
   } else {
